55e9248c650ae81d4e494edc2534b411f9b40ad7
alexisbarreaux <67143881+alexisbarreaux@users.noreply.github.com>
2023-01-16 10:35:39 +0100

    Added function to load back dumped dict

--- utils/jsonUtils.jl
+++ utils/jsonUtils.jl
@@ -7,3 +7,10 @@ function jsonDropToFile(filePath::String,data::Any)::Nothing
     end
     return
 end
+
+function jsonLoadDictFromFile(filePath::String)::Dict
+    open(filePath, "r") do file
+        parsedDict = JSON.parsefile(filePath; dicttype=Dict{String, Float64}, inttype=Int64, use_mmap=true)
+        return parsedDict
+    end
+end
