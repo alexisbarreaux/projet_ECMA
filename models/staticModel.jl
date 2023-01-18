93b45cb53dad227df6ec38f5f1849bed7a127981
alexisbarreaux <67143881+alexisbarreaux@users.noreply.github.com>
2023-01-18 11:40:45 +0100

    Correcting static

--- models/staticModel.jl
+++ models/staticModel.jl
@@ -52,17 +52,17 @@ function staticSolve(inputFile::String, showResult::Bool= false, silent::Bool=tr
     end
 
     # Variables
-    @variable(model, x[i in 1:n, j in 1:n], Bin)
+    @variable(model, x[i in 1:n, j in i+1:n], Bin)
     @variable(model, y[i in 1:n, k in 1:K], Bin)
 
     # Objective
-    @objective(model, Min, sum(x[i,j] * l[i,j] for i in 1:n for j in 1:n if i != j))
+    @objective(model, Min, sum(x[i,j] * l[i,j] for i in 1:n for j in i+1:n))
     
     # Constraints
     # Weights of the parts
     @constraint(model, [k in 1:K], sum(w_v[i]*y[i,k] for i in 1:n) <= B)
     # Triangular inequalities between x and y
-    @constraint(model, [k in 1:K, i in 1:n, j in i:n], y[i,k] + y[j,k] <= x[i,j] + 1)
+    @constraint(model, [k in 1:K, i in 1:n, j in i+1:n], y[i,k] + y[j,k] <= x[i,j] + 1)
     # Each node is in a part
     @constraint(model, [i in 1:n],  sum(y[i,k] for k in 1:K) == 1)
 
