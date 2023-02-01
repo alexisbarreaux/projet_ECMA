473c6f59c51bfeb02515d9ec6874c431e0041c82
NelusBreizh <sulian.lbc@orange.fr>
2023-02-02 00:28:42 +0100

    Heuristic corrected and optimized

--- heuristic/heuristic.jl
+++ heuristic/heuristic.jl
@@ -55,14 +55,14 @@ function check_feasibility(inputFile::String, solution::Dict{Int,Array})::Bool
     for (_, cluster) in solution
         worst_weight = compute_worst_weight(inputFile, cluster)
         if worst_weight > B
-            println("Worst weight : ", worst_weight, ", B : ", B)
+            #println("Worst weight : ", worst_weight, ", B : ", B)
             return false
         end
     end
     return true
 end
 
-function compute_worst_case(inputFile::String, solution::Dict{Int,Array})::Any
+function compute_worst_case(inputFile::String, solution::Dict{Int,Array}, mode::Int=0)::Any
     include(DATA_DIR_PATH * "\\" * inputFile)
     l = computeDistances(coordinates)
     total_distance = 0
@@ -70,10 +70,37 @@ function compute_worst_case(inputFile::String, solution::Dict{Int,Array})::Any
     for (_, cluster) in solution
         for a in cluster
             for b in cluster
-                if a != b
+                if a < b
                     append!(supplementary_distances, lh[a] + lh[b])
                     total_distance += l[a, b]
                 end
+            end
+        end
+    end
+    supplementary_distances = reverse!(sort(supplementary_distances))
+    remaining_uncertainty = L
+    robust_cost = 0
+    for d in supplementary_distances
+        error = min(3, remaining_uncertainty)
+        remaining_uncertainty -= error
+        robust_cost += error * d
+    end
+    if mode > 0
+        return total_distance + robust_cost, supplementary_distances, robust_cost, l
+    end
+    return total_distance + robust_cost
+end
+
+function compute_score_cluster(inputFile::String, cluster::Array)::Any
+    include(DATA_DIR_PATH * "\\" * inputFile)
+    l = computeDistances(coordinates)
+    total_distance = 0
+    supplementary_distances = []
+    for a in cluster
+        for b in cluster
+            if a != b
+                append!(supplementary_distances, lh[a] + lh[b])
+                total_distance += l[a, b]
             end
         end
     end
@@ -159,6 +186,7 @@ end
 end"""
 
 function run_heuristic(inputFile::String)::Union{Nothing,Tuple{Float64,Float64}}
+    println("Solving instance ", inputFile)
     start = time()
     best_solution = Dict{Int,Array}()
     best_res = -1
@@ -182,9 +210,13 @@ function run_heuristic(inputFile::String)::Union{Nothing,Tuple{Float64,Float64}}
         println("Heuristic solution for ", inputFile, " is not feasible in the worst case scenario")
         return
     end
-    println("Mode ", best_mode, " is the best one")
+    #println("Mode ", best_mode, " is the best one")
+    solution = local_search_bis(inputFile, best_solution)
+    if !(check_feasibility(inputFile, solution))
+        println("Local search result is not feasible for instance ", inputFile)
+    end
     runTime = time() - start
-    return runTime, best_res
+    return runTime, compute_worst_case(inputFile, solution)
 end
 
 function run_all_instances()::Nothing
@@ -206,4 +238,160 @@ function find_seed(inputFile::String)::Any
     end
     println("rip")
     return
+end
+
+function local_search_one_iteration(inputFile::String, solution::Dict{Int,Array})::Bool
+    include(DATA_DIR_PATH * "\\" * inputFile)
+    best_score = compute_worst_case(inputFile, solution)
+    best_switch = (0, 0, 0, 0)
+    for (a, cluster_a) in solution
+        for (b, cluster_b) in solution
+            if a != b
+                for i in 1:length(cluster_a)
+                    for j in 1:length(cluster_b)
+                        inter = cluster_a[i]
+                        cluster_a[i] = cluster_b[j]
+                        cluster_b[j] = inter
+                        if (compute_worst_weight_sort(inputFile, cluster_a) < B) && (compute_worst_weight_sort(inputFile, cluster_b) < B)
+                            new_score = compute_worst_case(inputFile, solution)
+                            if (new_score < best_score)
+                                best_score = new_score
+                                best_switch = (a, b, i, j)
+                            end
+                        end
+                        inter = cluster_a[i]
+                        cluster_a[i] = cluster_b[j]
+                        cluster_b[j] = inter
+                    end
+                end
+            end
+        end
+    end
+    if best_switch == (0, 0, 0, 0)
+        return false
+    else
+        (a, b, i, j) = best_switch
+        inter = solution[a][i]
+        solution[a][i] = solution[b][j]
+        solution[b][j] = inter
+        return true
+    end
+end
+
+function compute_static_distance_difference(distances::Any, cluster_a::Array, cluster_b::Array, i::Int, j::Int)::Float64
+    remove = 0
+    add = 0
+    for k in 1:length(cluster_a)
+        if k != i
+            remove += distances[cluster_a[k], cluster_a[i]]
+            add += distances[cluster_a[k], cluster_b[j]]
+        end
+    end
+    for l in 1:length(cluster_b)
+        if l != j
+            remove += distances[cluster_b[l], cluster_b[j]]
+            add += distances[cluster_b[l], cluster_a[i]]
+        end
+    end
+    return add - remove
+end
+
+function compute_robust_distance_difference(inputFile::String, supplementary_distances::Vector{Any}, robust_cost::Int, cluster_a::Array, cluster_b::Array, i::Int, j::Int)::Any
+    include(DATA_DIR_PATH * "\\" * inputFile)
+    copy_supplementary_distances = copy(supplementary_distances)
+    for k in 1:length(cluster_a)
+        if k != i
+            deleteat!(copy_supplementary_distances, indexin(lh[cluster_a[k]] + lh[cluster_a[i]], copy_supplementary_distances))
+            append!(copy_supplementary_distances, lh[cluster_a[k]] + lh[cluster_b[j]])
+        end
+    end
+    for l in 1:length(cluster_b)
+        if l != j
+            deleteat!(copy_supplementary_distances, indexin(lh[cluster_b[l]] + lh[cluster_b[j]], copy_supplementary_distances))
+            append!(copy_supplementary_distances, lh[cluster_b[l]] + lh[cluster_a[i]])
+        end
+    end
+    copy_supplementary_distances = reverse!(sort(copy_supplementary_distances))
+    new_robust_cost = 0
+    remaining_uncertainty = L
+    for d in copy_supplementary_distances
+        error = min(3, remaining_uncertainty)
+        remaining_uncertainty -= error
+        new_robust_cost += error * d
+    end
+    return new_robust_cost - robust_cost, copy_supplementary_distances
+end
+
+function local_search_one_iteration_bis(inputFile::String, distances::Any, solution::Dict{Int,Array}, supplementary_distances::Vector{Any}, robust_cost::Int)::Any
+    include(DATA_DIR_PATH * "\\" * inputFile)
+    best_static_gap = 0
+    best_robust_gap = 0
+    best_set_distances = []
+    best_switch = (0, 0, 0, 0)
+    for (a, cluster_a) in solution
+        for (b, cluster_b) in solution
+            if a != b
+                for i in 1:length(cluster_a)
+                    for j in 1:length(cluster_b)
+                        inter = cluster_a[i]
+                        cluster_a[i] = cluster_b[j]
+                        cluster_b[j] = inter
+                        if (compute_worst_weight_sort(inputFile, cluster_a) <= B) && (compute_worst_weight_sort(inputFile, cluster_b) <= B)
+                            inter = cluster_a[i]
+                            cluster_a[i] = cluster_b[j]
+                            cluster_b[j] = inter
+                            static_gap = compute_static_distance_difference(distances, cluster_a, cluster_b, i, j)
+                            robust_gap, set_distances = compute_robust_distance_difference(inputFile, supplementary_distances, robust_cost, cluster_a, cluster_b, i, j)
+                            if (static_gap + robust_gap < best_static_gap + best_robust_gap)
+                                best_static_gap = static_gap
+                                best_robust_gap = robust_gap
+                                best_set_distances = set_distances
+                                best_switch = (a, b, i, j)
+                            end
+                        else
+                            inter = cluster_a[i]
+                            cluster_a[i] = cluster_b[j]
+                            cluster_b[j] = inter
+                        end
+                    end
+                end
+            end
+        end
+    end
+    if best_switch == (0, 0, 0, 0)
+        return false, [], 0
+    else
+        (a, b, i, j) = best_switch
+        inter = solution[a][i]
+        solution[a][i] = solution[b][j]
+        solution[b][j] = inter
+        robust_cost += best_robust_gap
+        println(best_static_gap + best_robust_gap)
+        return true, best_set_distances, robust_cost
+    end
+end
+
+function local_search(inputFile::String, solution::Dict{Int,Array})::Dict{Int,Array}
+    include(DATA_DIR_PATH * "\\" * inputFile)
+    start = time()
+    iteration_count = 0
+    while local_search_one_iteration(inputFile, solution) && time() - start < 300
+        iteration_count += 1
+    end
+    println("Number of Local_search iterations : ", iteration_count)
+    return solution
+end
+
+function local_search_bis(inputFile::String, solution::Dict{Int,Array})::Dict{Int,Array}
+    include(DATA_DIR_PATH * "\\" * inputFile)
+    start = time()
+    _, supplementary_distances, robust_cost, distances = compute_worst_case(inputFile, solution, 1)
+    iteration_count = 1
+    change, supplementary_distances, robust_cost = local_search_one_iteration_bis(inputFile, distances, solution, supplementary_distances, robust_cost)
+    while change && time() - start < 300
+        iteration_count += 1
+        change, supplementary_distances, robust_cost = local_search_one_iteration_bis(inputFile, distances, solution, supplementary_distances, robust_cost)
+    end
+    println("Number of Local_search iterations : ", iteration_count)
+    return solution
 end
